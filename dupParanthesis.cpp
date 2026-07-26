afefc68c730fca23762693d77dc48ee902156eb9
Priyanshi-Raj95 <priyanshi.raj9595@gmail.com>
2026-07-26 11:34:53 +0530

    Duplicate Paranthesis

---
+++ dupParanthesis.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+bool isDuplicate(string str){
+    stack <char>s;
+    for(int i=0;i<str.size();i++){
+        char ch=str[i];
+        if(ch!=')'){ //non-closing
+            s.push(ch);
+        }else{ //closing
+            if(s.top()=='('){
+                return true;//DUPLICATE
+            }
+            while(s.top()!='('){
+                s.pop();
+            }
+            s.pop();
+        }
+    }
+    return false;
+}
+int main(){
+    string str="((a+b)+(c+d))";
+    cout<<isDuplicate(str);
+    return 0;
+}
