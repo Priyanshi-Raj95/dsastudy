cca5cae0ded9e2662355c6a0985b75bff52c98ed
Priyanshi-Raj95 <priyanshi.raj9595@gmail.com>
2026-08-21 00:27:13 +0530

    Indian coins

---
+++ indian_coins.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getMinChange(vector<int>coins, int v){
+    int ans=0;
+    int n=coins.size();
+    for(int i=n-1;i>=0 && v > 0;i--){
+        if(v>=coins[i]){
+            ans+=v/coins[i];
+            v=v%coins[i];
+        }
+    }
+    cout<<"Min coins for change : "<<ans<<endl;
+    return ans;
+}
+int main(){
+    vector<int>coins={1,2,5,10,20,50,100,500,2000};
+    int v=599;
+
+    getMinChange(coins,v);
+    return 0;
+}
