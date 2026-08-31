5af86a02dbfa31efd03d12c30f8368dedde60930
Priyanshi-Raj95 <priyanshi.raj9595@gmail.com>
2026-08-31 18:55:25 +0530

    Diameter of tree

---
+++ diameterTree.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data){
+        this->data=data;
+        left=right=NULL;
+    }
+};
+static int idx=-1;
+Node *buildTree(vector<int>nodes){
+    idx++;
+    if(nodes[idx]==-1){
+        return NULL;
+    }
+    Node* currNode= new Node( nodes[idx]); 
+    currNode->left=buildTree(nodes); //left subtree
+    currNode->right=buildTree(nodes); //right subtree
+    return currNode;
+}
+int height(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int leftHt= height(root->left);
+    int rightHt= height(root->right);
+    int currHt= max(leftHt,rightHt) +1;
+    return currHt;
+}
+int diam(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int currDiam= height(root->left)+height(root->right)+1;
+    int leftDiam= diam(root->left);
+    int rightDiam= diam(root->right);
+
+    return max(currDiam, max(leftDiam,rightDiam));
+}
+pair<int,int> diam2(Node *root){
+    if(root == NULL){
+        return make_pair(0,0);
+    }
+    pair<int,int>leftInfo = diam2(root->left);
+    pair<int,int>rightInfo = diam2(root->right);
+    
+    int currDiam=leftInfo.second + rightInfo.second +1;
+    int finalDiam= max(currDiam, max(leftInfo.first, rightInfo.first));
+    int finalHt = max(leftInfo.second, rightInfo.second) + 1;
+
+    return make_pair(finalDiam, finalHt);
+}
+int main(){
+    vector<int>nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    Node* root=buildTree(nodes);
+    cout<<"Diameter = "<<diam(root)<<endl;
+    cout<<"Diameter = "<<diam2(root).first;
+    return 0;
+}
